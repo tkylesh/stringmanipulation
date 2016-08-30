01301e9cc71971e2974291aaa758c79b721dd93f
Taylor Shuff <tkylesh@gmail.com>
2016-08-30 18:17:02 -0500

    addedasdf lakj

--- strings.js
+++ strings.js
@@ -1,15 +1,23 @@
-function reversal() {
 
+var ltr = document.getElementById('ltrInput').value;
+var btn = document.getElementById('btn');
+
+btn.addEventListener("click", reversal);
+
+function reversal() {
+	var arr = ltr.split(' ');
+	var revStr = arr.reverse();
+	document.getElementById('output').innerHTML += revStr.join(' ');
+	
 }
+
 
 function alphabits() {
 
-}
-
+} 
 function palindrome() {
 
 }
-
 var testString = "";
 reversal(testString);
 alphabits(testString);
