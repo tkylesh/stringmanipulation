1d01c285a76443f5b6cb762657696b1764b56723
Taylor Shuff <tkylesh@gmail.com>
2016-09-01 09:53:04 -0500

    wired up alphabit and palindrome buttons

--- strings.js
+++ strings.js
@@ -1,6 +1,8 @@
 
 // var ltr = document.getElementById('ltrInput').value;
 var reverse = document.getElementById('reverse');
+var alphabit = document.getElementById('alphabit');
+var palindrome = document.getElementById('palindrome');
 // var out = document.getElementById('output');
 
 reverse.addEventListener("click", reversal);
@@ -12,17 +14,15 @@ function reversal() {
 	// console.log(revArr.join(' '));
 	document.getElementById('output').innerHTML += `<li>${revArr.join(' ')}</li>`;
 };
-
-
-
-
+alphabit.addEventListener("click",function(){console.log("alphabit btn clicked!")});
 function alphabits() {
 
 } 
+palindrome.addEventListener("click",function(){console.log("palindrome btn clicked!")});
 function palindrome() {
 
 }
- var testString = "It's on like Donkey Kong";
+ var testString = "Donkey Kong";
  //reversal(testString);
-alphabits(testString);
-palindrome(testString);
+// alphabits(testString);
+// palindrome(testString);
